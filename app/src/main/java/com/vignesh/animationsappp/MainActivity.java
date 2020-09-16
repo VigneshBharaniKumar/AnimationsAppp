7cf45e0ac38bde0975b8c7dd8f71c6d645c33b50
VigneshBharaniKumar <vignesh.b.qa@gmail.com>
2020-09-16 21:52:30 +0530

    Added common OnClickListener and txt_rounded_bg for text view Added logic on fade in and fade out two different textViews

--- app/src/main/java/com/vignesh/animationsappp/MainActivity.java
+++ app/src/main/java/com/vignesh/animationsappp/MainActivity.java
@@ -1,17 +1,16 @@
 package com.vignesh.animationsappp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView txtHelloWorld;
+    private TextView txtHiWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +18,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         txtHelloWorld = findViewById(R.id.txtHelloWorld);
+        txtHiWorld = findViewById(R.id.txtHiWorld);
 
-        txtHelloWorld.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        txtHelloWorld.setOnClickListener(onClickListener);
+        txtHiWorld.setOnClickListener(onClickListener);
 
-                Toast.makeText(MainActivity.this, "Animate!", Toast.LENGTH_SHORT).show();
+    }
 
-                txtHelloWorld.animate().alpha(0f).setDuration(5000);
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if (txtHelloWorld.getAlpha() == 1) {
+
+                Toast.makeText(MainActivity.this, "Hi World", Toast.LENGTH_SHORT);
+                txtHelloWorld.animate().alpha(0).setDuration(3000);
+                txtHiWorld.animate().alpha(1).setDuration(3000);
+
+            } else {
+
+                Toast.makeText(MainActivity.this, "Hello World", Toast.LENGTH_SHORT);
+                txtHelloWorld.animate().alpha(1).setDuration(3000);
+                txtHiWorld.animate().alpha(0).setDuration(3000);
+
+            }
+
+            /*Toast.makeText(MainActivity.this, "Animate!", Toast.LENGTH_SHORT).show();
+
+            txtHelloWorld.animate().alpha(0).setDuration(3000);
+            txtHiWorld.animate().alpha(1).setDuration(3000);*/
 
                 /*txtHelloWorld.startAnimation(animation);
 
@@ -37,8 +57,7 @@ public class MainActivity extends AppCompatActivity {
                 txtHelloWorld.setText("Hi World");
                 txtHelloWorld.startAnimation(animation);*/
 
-            }
-        });
+        }
+    };
 
-    }
 }
